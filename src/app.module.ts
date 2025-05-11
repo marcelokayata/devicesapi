21d6d787d34edc9a3f010cac23497882746c9b5c
Marcelo Apovian Kayata <marcelo_kayata@hotmail.com>
2025-05-11 14:27:25 -0300

    clean: remove users  tutorial base rest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DevicesModule } from './devices/devices.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb://localhost:27017/crud-mongo"),
-    UsersModule,
     DevicesModule],
   controllers: [AppController],
   providers: [AppService],
